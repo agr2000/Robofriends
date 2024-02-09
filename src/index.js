26169a6a2995ee597497b300f0be119260cf0e63
Andrew Rodriguez <142957436+agr2000@users.noreply.github.com>
2024-02-08 19:55:59 -0800

    Add files via upload
    Robofriends app created from Zero-to-Mastery.

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+//import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
+import 'tachyons';
+import { robots } from './robots';
+import App from './containers/App';
+
+//ReactDOM.createRoot(<Hello />, document.getElementById('root'));
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+            <div>
+             <App /> 
+            </div>
+          );
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log)000)
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
